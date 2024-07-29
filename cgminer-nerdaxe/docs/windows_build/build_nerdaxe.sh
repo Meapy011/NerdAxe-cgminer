a6e52df288a86da9587691d9bc7b3b4765384f01
Meapy011 <meap09121@gmail.com>
2024-07-28 18:31:06 -0700

    Removed cgminer-nerdaxe

--- cgminer-nerdaxe/docs/windows_build/build_nerdaxe.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export folder=$(pwd)
-cd ~
-git clone https://github.com/nerdaxe/cgminer-nerdaxe.git
-cd cgminer-nerdaxe
-autoreconf -fi
-CFLAGS="-O2 -msse2" ./configure --host=i686-w64-mingw32.static --disable-shared --enable-nerdaxe
-make
-strip cgminer.exe
